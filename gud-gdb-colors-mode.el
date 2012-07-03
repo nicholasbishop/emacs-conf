2a2f7e32094079607697d888fe0e1e41f8bc6052
Nicholas Bishop <nicholasbishop@gmail.com>
2012-07-03 18:19:13 -0400

    Add gud-gdb-colors-mode

---
+++ gud-gdb-colors-mode.el
@@ -0,0 +1,9 @@
+(define-derived-mode gud-gdb-colors-mode gud-mode
+  (setq font-lock-defaults
+		'((;; function names
+		   ("\s *\\([a-zA-Z_]\\(\\sw\\|_\\)*\\) \(" 1 font-lock-function-name-face)
+		   ;; line numbers
+		   (":\\([0-9]*\\)" 1 font-lock-type-face)) t))
+  
+  (setq mode-name "gud-gdb-colors-mode")
+)
