ec0a93921741507bc76d23c4064fc2b5d2c500c1
Nicholas Bishop <nicholasbishop@gmail.com>
2012-12-06 11:01:20 -0500

    Adding makerbot startup configuration

---
+++ makerbot.el
@@ -0,0 +1,22 @@
+(add-to-list 'load-path "/home/nicholasbishop/emacs-conf/")
+(load "top")
+
+;; don't use tabs
+(setq-default indent-tabs-mode nil)
+
+;; header files (*.h) as C++ by default
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(defun remove-obj-dir (filename)
+  (replace-regexp-in-string
+   "obj/" "" filename))
+
+(setq compilation-parse-errors-filename-function 'remove-obj-dir)
+
+;; default to Google C++ style
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+
+;; start a shell
+(cd "/d/src/makerbot")
+(shell)
