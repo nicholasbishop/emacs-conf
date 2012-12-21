3e592700dc44c76d66772d8e27ab00a4cc8df149
Nicholas Bishop <nicholasbishop@gmail.com>
2012-12-21 10:36:26 -0500

    Fix gud-gdb highlighting for C++ methods

--- gud-gdb-colors-mode.el
+++ gud-gdb-colors-mode.el
@@ -1,7 +1,7 @@
 (define-derived-mode gud-gdb-colors-mode gud-mode
   (setq font-lock-defaults
 		'((;; function names
-		   ("\s *\\([a-zA-Z_]\\(\\sw\\|_\\)*\\) \(" 1 font-lock-function-name-face)
+		   ("\s *\\([a-zA-Z_]\\(\\sw\\|_\\|:\\)*\\) \(" 1 font-lock-function-name-face)
 		   ;; line numbers
 		   (":\\([0-9]*\\)" 1 font-lock-type-face)) t))
   
