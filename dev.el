ab2dc4007471349ab23c35edbceab205538ff4d8
Nicholas Bishop <nicholasbishop@gmail.com>
2013-06-08 17:01:15 -0400

    Fix C++11 enum class indentation

--- dev.el
+++ dev.el
@@ -171,3 +171,38 @@
 
 ;; Rust
 (require 'rust-mode)
+
+;; Namespace insertion
+(defun cxx-insert-namespace ()
+  (interactive)
+  (insert "namespace " (read-string "Namespace: ")))
+
+(global-set-key "\C-zn" 'cxx-insert-namespace)
+
+;; C++11 fixes from stackoverflow.com/questions/6497374
+(defun inside-class-enum-p (pos)
+  "Checks if POS is within the braces of a C++ \"enum class\"."
+  (ignore-errors
+    (save-excursion
+      (goto-char pos)
+      (up-list -1)
+      (backward-sexp 1)
+      (looking-back "enum[ \t]+class[ \t]+[^}]*"))))
+
+(defun align-enum-class (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      0
+    (c-lineup-topmost-intro-cont langelem)))
+
+(defun align-enum-class-closing-brace (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      '-
+    '+))
+
+(defun fix-enum-class ()
+  "Setup `c++-mode' to better handle \"class enum\"."
+  (add-to-list 'c-offsets-alist '(topmost-intro-cont . align-enum-class))
+  (add-to-list 'c-offsets-alist
+               '(statement-cont . align-enum-class-closing-brace)))
+
+(add-hook 'c++-mode-hook 'fix-enum-class)
