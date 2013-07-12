1fbeca45cb3bbbf828cd726e478422e41ad0e305
Nicholas Bishop <nicholas.bishop@makerbot.com>
2013-07-12 11:12:04 -0400

    Set better ff-search-directories

--- dev.el
+++ dev.el
@@ -35,6 +35,14 @@
 
 (push "\\*compil.*" same-window-regexps)
 
+;; set better include search paths
+(setq-default ff-search-directories
+      '("."
+        "/usr/include"
+        "/usr/local/include"
+        "/usr/include/c++/4.8"
+        "../*/"))
+
 ;; key to switch between header and src
 (global-set-key "\C-zh" 'ff-find-other-file)
 
