8e5fc50c830c92f1bc6b2c0547602d0d4a966b9d
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-11 13:56:09 -0400

    Make C-xm do the same thing as C-xC-m

--- top.el
+++ top.el
@@ -57,6 +57,7 @@
 (tool-bar-mode 0)
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-xm" 'execute-extended-command)
 
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
