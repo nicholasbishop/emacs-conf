3b71d6dc4e5c90e49c32831f258f5583d20a1619
Nicholas Bishop <nbishop@nbishop.net>
2026-02-01 17:51:24 -0500

    jjb: Add LineNumber

--- jjb/src/main.rs
+++ jjb/src/main.rs
@@ -14,11 +14,24 @@ enum Action {
     BlameLine { path: PathBuf, line: usize },
 }
 
-fn blame_line(path: &Path, line: usize) -> Result<()> {
-    let line = line
-        .checked_sub(1)
-        .context("line number must be greater than zero")?;
+#[derive(Clone, Copy)]
+struct LineNumber(usize);
 
+impl LineNumber {
+    fn new(index_from_one: usize) -> Result<Self> {
+        if index_from_one == 0 {
+            bail!("line number must be greater than zero");
+        } else {
+            Ok(Self(index_from_one - 1))
+        }
+    }
+
+    fn index_from_zero(&self) -> usize {
+        self.0
+    }
+}
+
+fn blame_line(path: &Path, line: LineNumber) -> Result<()> {
     let cmd_str = format!("`jj file annotate {}`", path.display());
     let output = Command::new("jj")
         .args(["file", "annotate"])
@@ -33,7 +46,10 @@ fn blame_line(path: &Path, line: usize) -> Result<()> {
     let stdout = str::from_utf8(&output.stdout)
         .context("annotate output is not UTF-8")?;
 
-    let line = stdout.lines().nth(line).context("invalid line number")?;
+    let line = stdout
+        .lines()
+        .nth(line.index_from_zero())
+        .context("invalid line number")?;
     let (rev, _) = line
         .split_once(" ")
         .context("missing separator in output")?;
@@ -46,6 +62,8 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     match &args.action {
-        Action::BlameLine { path, line } => blame_line(path, *line),
+        Action::BlameLine { path, line } => {
+            blame_line(path, LineNumber::new(*line)?)
+        }
     }
 }
