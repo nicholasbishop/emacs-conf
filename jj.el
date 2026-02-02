cc57fcd5009be0f3ad779700ebefcf9202e60a6e
Nicholas Bishop <nbishop@nbishop.net>
2026-02-01 21:11:13 -0500

    jj: Add command to get buffer path within the repo

--- jj.el
+++ jj.el
@@ -41,6 +41,26 @@
 
       ;; Show the buffer.
       (display-buffer bufname))))
+
+(defun jj-root ()
+  "Search upwards from cwd for jj root."
+  (substring (shell-command-to-string "jj root") 0 -1))
+
+(defun jj-buffer-path ()
+  "Add buffer's file path relative to git root to the kill ring"
+  (interactive)
+  (let ((path (file-relative-name
+               ;; buffer-filename returns an absolute path but not
+               ;; necessarily a canonical path in that symlinks are
+               ;; not resolved, whereas git-root-dir does implicitly
+               ;; resolve symlinks because that's the behavior of `git
+               ;; rev-parse --show-toplevel`. To make
+               ;; file-relative-name work as expected, call
+               ;; file-truename on the buffer name to canonicalize it.
+               (file-truename (buffer-file-name))
+               (jj-root))))
+	(message path)
+	(kill-new path)))
       
 (global-unset-key "\C-j")
 
@@ -49,3 +69,6 @@
 
 (global-set-key "\C-jd" 'jj-diff)
 (global-set-key "\C-j\C-d" 'jj-diff)
+
+(global-set-key "\C-jf" 'jj-buffer-path)
+(global-set-key "\C-j\C-f" 'jj-buffer-path)
